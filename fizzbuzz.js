3d562d0fca17e0d8894d71ba1a2cb0ed6024105d
ZOENG <Zoe.Ng@softwire.com>
2024-07-02 11:43:19 +0100

    Part 2c

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,19 @@
 // This is our main function
 function fizzbuzz() {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 200; i++) {
         let text = "";
         if(i%11 == 0){
-            text = "Bong";
+            if(i%13 == 0){
+                text = "FezzBong";
+            }else {
+                text = "Bong";
+            }
         } else {
             if(i%3 == 0){
                 text += "Fizz";
+            }
+            if(i%13 == 0){
+                text += "Fezz";
             }
             if(i%5 == 0){
                 text += "Buzz";
